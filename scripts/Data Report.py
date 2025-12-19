4d4ab19c36fd0755a9a78f0a5543c81787e7e439
Charles Ekanem <charlesitaekanem@gmail.com>
2025-12-19 21:59:30 +0100

    Delete scripts directory

--- scripts/Data Report.py
+++
@@ -1,9 +0,0 @@
-#Data Reporter
-
-def generate_data_report(csv_path):
-    df = pd.read_csv(csv_path)
-    print("Summary:\n")
-    print(df.describe(include='all'))
-    df[label_column].value_counts().plot(kind='bar', title='Class Distribution').figure.savefig('class_distribution.png')
-
-# generate_data_report('data.csv')
